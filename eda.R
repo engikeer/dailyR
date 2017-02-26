e7b337927728235d7ae7baad3e63c4ce8f6c6dd8
engikeer <engikeer@163.com>
2017-02-26 15:05:34 +0800

    add function sk

--- eda.R
+++ eda.R
@@ -1,10 +1,20 @@
 EDA <- function(x) {
-  # 探索性数据分析方法
-  par(mfrow = c(2, 2))  # 每页4个图
-  hist(x)	# 直方图
-  dotchart(x)	#点图
-  boxplot(x, horizontal = T) 	# 水平箱线图
-  qqnorm(x); qqline(x)		#正态概率图
-  par(mfrow = c(1, 1))		# 恢复每页1图
+    # 探索性数据分析方法
+    par(mfrow = c(2, 2))  # 每页4个图
+    hist(x)	# 直方图
+    dotchart(x)	#点图
+    boxplot(x, horizontal = T) 	# 水平箱线图
+    qqnorm(x); qqline(x)		#正态概率图
+    par(mfrow = c(1, 1))		# 恢复每页1图
+}
+sk <- function(x, na.omit=FALSE) {
+    if (na.omit)
+        x <- x[!is.na(x)]
+    m <- mean(x)
+    n <- length(x)
+    s <- sd(x)
+    skew <- sum((x-m)^3/s^3)/n
+    kurt <- sum((x-m)^4/s^4)/n - 3
+    return(c(n=n, mean=m, stdev=s, skew=skew, kurtosis=kurt))
 }
 
