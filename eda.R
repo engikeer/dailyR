32b8a64f5924fa83ee94ea466ed82ece0e9338f9
engikeer <engikeer@163.com>
2017-02-26 14:52:15 +0800

    add ead.R

---
+++ eda.R
@@ -0,0 +1,9 @@
+EDA <- function(x) {
+  # 探索性数据分析方法
+  par(mfrow = c(2, 2))  # 每页4个图
+  hist(x)	# 直方图
+  dotchart(x)	#点图
+  boxplot(x, horizontal = T) 	# 水平箱线图
+  qqnorm(x); qqline(x)		#正态概率图
+  par(mfrow = c(1, 1))		# 恢复每页1图
+}
